160a953a693a84142002416b20039b68d1532194
Vishnu Ganesan <vishnukg@gmail.com>
2026-05-18 20:45:05 +1000

    Fix polybar

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u "$UID" -x polybar > /dev/null; do sleep 0.1; done
 # Launch bar on each connected monitor
 if type xrandr > /dev/null 2>&1; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR="$m" GTK_ICON_THEME=Papirus polybar --reload main 2>&1 | tee -a /tmp/polybar-"$m".log &
+        MONITOR="$m" GTK_ICON_THEME=Papirus polybar main 2>&1 | tee -a /tmp/polybar-"$m".log &
     done
 else
     polybar --reload main &
